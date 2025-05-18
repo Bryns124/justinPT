8dfaa90cb9872a602454af621605e386f34a66ea
Bryns124 <bryan.le1248@gmail.com>
2025-05-18 21:25:37 +1000

    restarting setup

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 const port = 3000;
+const uri = "mongodb+srv://Cluster92906:SnF4b3ZWanVS@trainerapp.s0tb2mi.mongodb.net/?retryWrites=true&w=majority&appName=trainerApp"
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Successfully connected to MongoDB Atlas");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connect();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
